4a8f12827f8c9859cb3856264b78ab03c7307835
craus <craus902@gmail.com>
2016-11-23 01:17:32 +0400

    UI; BlackMage health panel

---
+++ Assets/Scripts/SliderWithValue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class SliderWithValue : MonoBehaviour
+{
+    public Slider slider;
+    public Text valueText;
+    public string formatString;
+
+    void Update() {
+        valueText.text = string.Format(formatString, slider.value);
+    }
+}
