7826fdfb9a8792192b70021570a9c717a8b6ce7e
craus <pavel.semushin@cloudcastlegroup.com>
2017-05-02 23:32:16 +0400

    effects; tilt

--- Assets/Scripts/UI/SpawnCounter.cs
+++ Assets/Scripts/UI/SpawnCounter.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Linq;
 
 [ExecuteInEditMode]
 public class SpawnCounter : MonoBehaviour
 {
+    public Tilt tilt;
     public Slider slider;
     public Spawner spawner;
     public int maxValue;
 
     void Update() {
-        slider.value = spawner.spawnedObjects.Count;
+        slider.value = spawner.spawnedObjects.Count(go => go.activeInHierarchy);
         slider.maxValue = maxValue;
+        if (tilt != null) {
+            tilt.Switch(slider.value == slider.maxValue);
+        }
     }
 }
---
+++ Assets/Scripts/UI/Tilt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Tilt : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public RawImage image;
+
+    public void Switch(bool on = true) {
+        image.enabled = on;
+        audioSource.mute = !on;
+    }
+}
