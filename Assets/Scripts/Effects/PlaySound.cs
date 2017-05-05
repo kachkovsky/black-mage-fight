efd124f5abde1a6922056136f133568608e18891
craus <pavel.semushin@cloudcastlegroup.com>
2017-05-05 05:38:55 +0400

    pshhhhh

--- Assets/Scripts/Effects/PlaySound.cs
+++ Assets/Scripts/Effects/PlaySound.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class PlaySound : Effect
 {
     public AudioSource target;
+    public float last = float.PositiveInfinity;
 
     public override void Run() {
+        target.time = target.clip.length - last;
         target.Play();
     }
 }
