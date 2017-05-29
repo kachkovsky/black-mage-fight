98a468a64225b20748a7eaa37048fecc190dc702
Pavel Semushin <pavel.semushin@castle.co>
2017-05-29 15:35:04 +0400

    reduced difficulty of Charge level for difficulty levels 0-2

--- Assets/Scripts/Conditions/CheckDifficulty.cs
+++ Assets/Scripts/Conditions/CheckDifficulty.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CheckDifficulty : Condition
 {
     public int difficulty = 0;
+    public List<int> diffuculties;
 
     public override bool Satisfied() {
+        if (diffuculties.Count > 0) {
+            return GameManager.instance.gameState.CurrentRun != null && diffuculties.Contains(GameManager.instance.gameState.CurrentRun.difficulty);
+        }
         return GameManager.instance.gameState.CurrentRun != null && GameManager.instance.gameState.CurrentRun.difficulty == difficulty;
     }
 }
