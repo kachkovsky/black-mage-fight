4b1f21b6ace235d0a93c545c151ee6bfd38fc9b9
Pavel Semushin <pavel.semushin@castle.co>
2019-04-05 05:46:08 +0400

    black mage relocator for skulls level

---
+++ Assets/Scripts/Effects/BlinkWithMark.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BlinkWithMark : Effect
+{
+	public List<Mark> marks;
+
+	public override void Run() {
+		FindObjectsOfType<Figure>().Where(f => f.Marked(marks)).ForEach(f => f.Relocate());
+	}
+}
