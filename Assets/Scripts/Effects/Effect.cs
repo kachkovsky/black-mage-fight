acdf545fa813c81eb27f3b805e7de278f7337c52
Pavel Semushin <pavel.semushin@castle.co>
2019-04-13 12:39:30 +0400

    blink from context menu; letter painter; letters+heartstopper balanced

--- Assets/Scripts/Effects/Effect.cs
+++ Assets/Scripts/Effects/Effect.cs
@@ -4,4 +4,10 @@ using System.Collections;
 public abstract class Effect : MonoBehaviour
 {
     public abstract void Run();
+
+	[ContextMenu("Run")]
+	public void RunFromEditor() {
+        Board.instance.Restore();
+		Run();
+	}
 }
---
+++ Assets/Scripts/Tokens/LetterPainter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Linq;
+using RSG;
+
+public class LetterPainter : MonoBehaviour
+{
+	public List<Color> colors;
+
+	public SpriteRenderer toPaint;
+
+	public void Start() {
+		var letter = GetComponent<Letter>();
+		var index = letter.checker.letterMarks.IndexOf(letter.marker.mark);
+		toPaint.color = colors[index];
+	}
+}
