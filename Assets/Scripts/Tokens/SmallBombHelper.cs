d1f1d067ad737f24f6082fcf8a0c9f86751ac43d
Pavel Semushin <pavel.semushin@castle.co>
2019-04-16 13:46:04 +0400

    monsters + timed bombs balanced

---
+++ Assets/Scripts/Tokens/SmallBombHelper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class SmallBombHelper : MonoBehaviour
+{
+	public SpriteRenderer sprite;
+	public SpriteRenderer dangerSprite;
+
+	public Counter counter;
+
+	public Explosive explosive;
+
+	public void Awake() {
+		counter = GetComponentInParent<Counter>();
+		explosive = GetComponentInParent<Explosive>();
+	}
+
+	public void Start() {
+		counter.onDecrement.AddListener(OnCounterDecrement);
+	}
+
+	public void OnCounterDecrement() {
+		var danger = (counter.value == 1);
+		sprite.gameObject.SetActive(!danger);
+		dangerSprite.gameObject.SetActive(danger);
+
+		//if (danger) {
+		//	explosive.ExplosionArea().ForEach(cell => {
+		//		cell.ChangeColor(Color.yellow);
+		//	});
+		//}
+	}
+}
