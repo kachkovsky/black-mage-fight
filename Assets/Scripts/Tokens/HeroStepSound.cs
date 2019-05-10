bd60602ef3a23e64cc83c56cbd25f768b7921d54
Pavel Semushin <pavel.semushin@castle.co>
2019-05-10 08:53:56 +0400

    hero step sound pitch x2 when with key

---
+++ Assets/Scripts/Tokens/HeroStepSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class HeroStepSound : MonoBehaviour
+{
+	public AudioSource sound;
+	public KeyCounter keyCounter;
+
+	public void Awake() {
+		sound = GetComponent<AudioSource>();
+		keyCounter = FindObjectOfType<KeyCounter>();
+	}
+
+	public float Pitch() {
+		if (DoorSpawner.instance) {
+			return keyCounter.counter.value > 0 ? 2 : 1;
+		}
+		return 1;
+	}
+
+	public void Update() {
+		sound.pitch = Pitch();
+	}
+}
