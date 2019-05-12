162cb02c3199ac9e4bdf4faa9325275499fb3fcc
Pavel Semushin <pavel.semushin@castle.co>
2019-05-12 16:57:21 +0400

    clock ui for periodic – used for doors

--- Assets/Scripts/UI/PeriodicUI.cs
+++ Assets/Scripts/UI/PeriodicUI.cs
@@ -7,7 +7,12 @@ public class PeriodicUI : MonoBehaviour
 {
 	public int n = 17;
 
-	public List<Transform> axes;
+	public List<PeriodicUIAxe> axes;
+
+	public Color basic;
+	public Color active;
+
+	public Transform arrow;
 
 	public void Awake() {
 		for (int i = 0; i < n - 1; i++) {
@@ -20,11 +25,16 @@ public class PeriodicUI : MonoBehaviour
 		for (int i = 0; i < axes.Count; i++) {
 			if (i < p.periodic.period) {
 				axes[i].gameObject.SetActive(true);
-				axes[i].rotation = Quaternion.Euler(Vector3.forward * (90 + 360 * i / p.periodic.period));
+				axes[i].transform.rotation = Quaternion.Euler(Vector3.forward * (90 - 360 * (i - 0.5f) / p.periodic.period));
+				axes[i].image.color = basic;
 			} else {
 				axes[i].gameObject.SetActive(false);
 			}
 		}
+		for (int i = 0; i < p.periodic.runPhases.Count; i++) {
+			axes[p.periodic.runPhases[i]].image.color = active;
+		}
+		arrow.rotation = Quaternion.Euler(Vector3.forward * (90 - 360 * p.periodic.phase / p.periodic.period));
 	}
 
 }
