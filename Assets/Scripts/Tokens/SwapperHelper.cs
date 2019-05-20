5a069c4184e269ce7e0d2b65e95be110fd9be0a0
Pavel Semushin <pavel.semushin@castle.co>
2019-05-20 12:59:53 +0400

    swapper helper (currently off); swapper+statues level balanced all difficulties

---
+++ Assets/Scripts/Tokens/SwapperHelper.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class SwapperHelper : Token
+{
+	public List<LineRenderer> lines;
+
+	void See(Cell cell, IntVector2 direction, LineRenderer line) {
+		var start = cell;
+		cell = cell.ToDirection(direction);
+		for (int i = 0; i < 100 && cell != null && !cell.figures.Any(f => f is Swapper && f.gameObject.activeSelf); i++) {
+            cell = cell.ToDirection(direction);
+        }
+		if (cell != null) {
+			line.enabled = true;
+			line.SetPosition(
+				0, 
+				((start.transform.position + start.ToDirection(direction).transform.position)/2)
+					.Change(z: -1)
+			);
+			line.SetPosition(
+				1, 
+				((cell.transform.position+cell.ToDirection(-direction).transform.position)/2)
+					.Change(z: -1));
+		} else {
+			line.enabled = false;
+		}
+    }
+
+    public void Run() {
+        See(Hero.instance.Position, new IntVector2(1, 0), lines[0]);
+        See(Hero.instance.Position, new IntVector2(-1, 0), lines[1]);
+        See(Hero.instance.Position, new IntVector2(0, 1), lines[2]);
+        See(Hero.instance.Position, new IntVector2(0, -1), lines[3]);
+    }
+}
---
+++ Assets/Scripts/Visual Effects/TexturedLine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class TexturedLine : MonoBehaviour
+{
+	public LineRenderer line;
+
+	public Vector2 moveSpeed = Vector2.zero;
+
+	public Vector2 tilesPerUnit = Vector2.one;
+
+	public void Awake() {
+		line = GetComponent<LineRenderer>();
+	}
+
+	public float Length() {
+		return (line.GetPosition(1) - line.GetPosition(0)).magnitude;
+	}
+
+	public void Update() {
+		line.material.SetTextureScale("_MainTex", new Vector2(Length()/line.widthMultiplier, 1).Scaled(tilesPerUnit));
+		line.material.SetTextureOffset("_MainTex", -moveSpeed * Time.time);
+	}
+}
